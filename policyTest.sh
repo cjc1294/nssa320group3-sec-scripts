379900dc723604a1c7fb312ff9d0b54be0ddc1e8
Chris Cheney <cjc1294@rit.edu>
2021-09-16 16:14:45 -0400

    Added firewall tests

--- policyTest.sh
+++ policyTest.sh
@@ -6,6 +6,10 @@
 # Machines Affected:
 #	Application server
 
+# Firewall services and ports
+expectedServices="dhcpv6 http ssh"
+expectedPorts=""
+
 # Get network information
 echo "### NETWORK INFORMATION ###"
 ip a
@@ -26,6 +30,20 @@ echo
 echo "### LISTENING PORTS ###"
 ss -l -t -u -p
 
+echo
+
 # Get firewalld restrictions
+echo "### FIREWALL RULES ###"
+services=$(firewall-cmd --list-all | grep "services" | -d ' ' -f4-)
+echo "Firewalld services: $services"
+if [[ $services != $expectedServices ]] then
+	echo "!!Services do not match expected services: $expectedServices!!"
+fi
+
+ports=$(firewall-cmd --list-all | grep " ports" | -d ' ' -f4-)
+echo "Firewalld ports: $ports"
+if [[ $ports != $expectedPorts ]] then
+	echo "!!Ports do not match expected ports: $expectedPorts!!"
+fi
 
 # Get information on installed packages
