163e4b72e37ba236e770fea3b8cbd1b8f22ddaf7
Chris Cheney <cjc1294@rit.edu>
2021-09-16 15:46:32 -0400

    Added open/established connections tests

--- policyTest.sh
+++ policyTest.sh
@@ -15,7 +15,16 @@ echo
 echo "### CONNECTIVITY TEST ###"
 ping google.com -c 2
 
+echo
+
 # Get open ports/established connections
+echo "### ESTABLISHED CONNECTIONS ###"
+ss -a -t -u -p | grep "ESTAB" | sed -r "s/\s+/    /g" # Get established connections, and use some sed replacement to clean up the whitespace
+
+echo
+
+echo "### LISTENING PORTS ###"
+ss -l -t -u -p
 
 # Get firewalld restrictions
 
