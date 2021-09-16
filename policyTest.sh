d1a5edd63d5c7cb3f246714b1ee1764ce2c2c837
Chris Cheney <cjc1294@rit.edu>
2021-09-16 15:00:23 -0400

    Basic script skeleton

---
+++ policyTest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Authors: Chris Cheney(cjc1294)
+# Purpose:
+#	Test the application servers to make sure they conform to the security policies
+# Machines Affected:
+#	Application server
+
+# Get network information
+
+# Get open ports/established connections
+
+# Get firewalld restrictions
+
+# Get information on installed packages
